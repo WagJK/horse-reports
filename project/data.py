995e36f347c8af01f55024ea562de78c8f5fce26
WagJK <wagjk@hku.hk>
2018-08-25 03:41:50 -0500

    update data_input

--- project/data.py
+++ project/data.py
@@ -1,11 +1,34 @@
-import bs4
+import pandas as pd
+import requests
+import pprint
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
+
+pp = pprint.PrettyPrinter()
+
+def pprint(table):
+    for row in table: print(row)
+
+
+def make_list(table):
+    result = []
+    allrows = table.findAll('tr')
+    for row in allrows:
+        result.append([])
+        allcols = row.findAll('td')
+        for col in allcols:
+            thestrings = [str(s).strip('\r\n ') for s in col.findAll(text=True)]
+            thetext = ''.join(thestrings)
+            result[-1].append(thetext)
+    return result
 
 
 def data_input(url):
     response = urlopen(url)
-    soup = BeautifulSoup(response)
-    for table in soup.find_all('table'):
-        print(table)
+    soup = BeautifulSoup(response, 'lxml')
+    table = make_list(soup.find_all('table')[4])
+    table = list(filter(lambda x: len(x) > 5, table))
+    pprint(table)
+    
 
 data_input('http://racing.hkjc.com/racing/info/meeting/Results/chinese/')
